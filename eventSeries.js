bc545c1d5c2a6388c7ef90801c35855a717fff3e
CSuperman <crlbusiness1@gmail.com>
2024-01-15 23:00:05 -0500

    Create eventSeries.js

---
+++ eventSeries.js
@@ -0,0 +1,34 @@
+const NAME = "billybob joe"
+
+class EventSeries {
+  constructor(events, name) {
+    this.events = events;
+    this.currentEventIndex = 0;
+    this.name = "billybob joe"
+  }
+
+  executeNextEvent() {
+    const eventKey = Object.keys(this.events)[this.currentEventIndex];
+    this.events[eventKey]();
+    this.currentEventIndex++;
+
+    if (this.currentEventIndex >= Object.keys(this.events).length) {
+      console.log("All events have been executed");
+      return;
+    }
+  }
+}
+
+const eventSeries = new EventSeries({
+  droppedOffHorse: () => console.log("Dropped off your horse in a swamp land"),
+  bittenByViper: () => console.log("${NAME} are bitten by a swamp viper!"),
+  playerDeath: () => console.log("${NAME} died...."),
+});
+
+document.addEventListener("keydown", () => {
+  eventSeries.executeNextEvent();
+});
+
+// Start
+eventSeries.executeNextEvent();
+

8d08ac6ffa51274fe5cd6e6927b76d03dfd38e82
CSuperman <crlbusiness1@gmail.com>
2024-02-16 22:13:03 -0500

    Update petEating.js
    bugs

--- petEating.js
+++ petEating.js
@@ -19,11 +19,17 @@ class Monster {
     this.effects.push(food.sideEffect);
     console.log(`${this.name} eats the ${food.name}!`);
     console.log(`Stat boost: ${JSON.stringify(food.statBoost)}`);
-    console.log(`Side effect: ${typeof food.sideEffect === 'function' ? food.sideEffect(this) : food.sideEffect.name}`);
+
   }
 
   dayEnd() {
-    this.effects.forEach((effect) => effect(this));
+    this.effects.forEach((effect) => {
+      if (typeof effect === 'function') {
+        effect(this);
+      } else {
+        console.log(`Side effect: ${effect}`); // Log strings instead of calling
+      }
+    });
     this.effects = [];
   }
 
@@ -40,21 +46,18 @@ class Monster {
 
 
 const foods = [
-  new Food("Eyeball Stew", { color: "yellow" }, { name: "O.M.G. I'm feeling kind of yellow, do I look yellow?" }),
-  new Food("Eyeball Stew", { strength: 1 }, { name: "Hallucinations" }),
-  new Food("Slime Surprise", { agility: 1 }, { name: "Slippery (Agility penalty)" }),
-  new Food("Brain Jello", { intelligence: 1 }, (monster) => console.log(`${monster.name} has a headache! (Intelligence penalty)`)),
-  new Food("Glowing Goo", { health: 2 }, (monster) => console.log(`${monster.name} feels radioactive! (Health penalty)`)),
-  new Food("Smooshed Grom", { strength: 2, agility: 2, health: 2 }, (monster) => console.log(`${monster.name} feels powerful`)),
+    new Food("Eyeball Stew", { color: "yellow" }, { strength: 1 }, `Hallucinations`),
+  new Food("Slime Surprise", { color: null }, { agility: 1 }, "Slippery (Agility penalty)"),
+  new Food("Brain Jello", { color: null }, { intelligence: 1 }, (monster) => console.log(`${monster.name} has a headache! (Intelligence penalty)`)),
+  new Food("Glowing Goo", { color: null }, { health: 2 }, (monster) => console.log(`${monster.name} feels radioactive! (Health penalty)`)),
+  new Food("Smooshed Grom", { color: null }, { strength: 2, agility: 2, health: 2 }, (monster) => console.log(`${monster.name} feels powerful`)),
 ];
 
-const monster = new Monster("Grompo", 5, 3, 2, 10);
+const monster = new Monster("Grompo");
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const food = foods[Math.floor(Math.random() * foods.length)];
 monster.eat(food);
-
-
 monster.dayEnd();
 console.log(monster);
